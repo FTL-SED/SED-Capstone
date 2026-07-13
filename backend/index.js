949c012081ea9bc865145de6ce404d5582352cfd
Semir Ali <alisemir822@gmail.com>
2026-07-13 14:35:18 -0700

    Set up the backend server + prisma

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+// Set up routes here
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
