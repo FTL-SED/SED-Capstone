def057cb390cef750610eee2fe4d360a6c0babc8
Dylan D'Rozario <drozari@bc.edu>
2026-07-29 13:14:48 -0700

    Thread meeting point and radius into stop panels

--- frontend/src/pages/ItineraryPage/ItineraryPanel/ItineraryPanel.jsx
+++ frontend/src/pages/ItineraryPage/ItineraryPanel/ItineraryPanel.jsx
@@ -14,7 +14,7 @@ function ItineraryPanel({
   activeTab, onTabChange,
   onToggleLike, onToggleBookmark, onTogglePrivacy,
   onDelete, onCopy, onMarkVisited,
-  onRemoveStop, onEditStop, onAddStop, onReorderStops,
+  onRemoveStop, onEditStop, onAddStop, meetingPoint, radiusMi, onReorderStops,
   onEditItinerary, actionBusy,
 }) {
   // Per-person total from the stops' prices. When it exceeds the trip's budget,
@@ -239,6 +239,8 @@ function ItineraryPanel({
           onRemoveStop={onRemoveStop}
           onEditStop={onEditStop}
           onAddStop={onAddStop}
+          meetingPoint={meetingPoint}
+          radiusMi={radiusMi}
           onReorderStops={onReorderStops}
         />
       </div>
